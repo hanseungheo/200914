4ed4adf077a0510f8c1a6210bcfd0ca9d8bf681e
hanseungheo <martinhan486@naver.com>
2020-09-29 21:20:45 +0900

    200922_MyApplication 텍스트뷰

--- app/src/main/java/com/hanseungheon/sample/MainActivity.kt
+++ app/src/main/java/com/hanseungheon/sample/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.hanseungheon.sample
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val TAG="myapplicationTAG"
+        var a=3
+        var b="아무거나"
+
+         a=5
+        Log.d(TAG,textview1.text.toString())
+        textview1.typeface= Typeface.createFromAsset(assets,"BMJUA_ttf.ttf")
+
+
 
     }
 }
