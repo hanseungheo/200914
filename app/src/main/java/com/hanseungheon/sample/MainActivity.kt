cc7d54599b6791c740e6c186515ffc480486dc1b
hanseungheo <martinhan486@naver.com>
2020-10-13 22:32:18 +0900

    201013_MyApplication                알림을 만들엇다

--- app/src/main/java/com/hanseungheon/sample/MainActivity.kt
+++ app/src/main/java/com/hanseungheon/sample/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.hanseungheon.sample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.graphics.Typeface
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val TAG="myapplicationTAG"
-        var a=3
-        var b="아무거나"
+        var channelid="아무거나"
+        val nm=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel=NotificationChannel(channelid,"아무거나", NotificationManager.IMPORTANCE_HIGH)
+        nm.createNotificationChannel(channel)
 
-         a=5
-        Log.d(TAG,textview1.text.toString())
-        textview1.typeface= Typeface.createFromAsset(assets,"BMJUA_ttf.ttf")
+        val builder=NotificationCompat.Builder(this,channelid)
+        builder.setContentTitle("로그캣")
+            .setContentText("알림이 도착했습니다")
+            .setSmallIcon(R.drawable.ic_audiotrack)
+            .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+
+        button.setOnClickListener {
+            nm.notify(512, builder.build())
+        }
+
 
 
 
